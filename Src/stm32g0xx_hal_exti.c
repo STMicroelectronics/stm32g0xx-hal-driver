bba42c0a41e0474906b1cd4d9fcf2b21a6743d7a
houssine BOUGUERBA <houssine.bougerba-ext@st.com>
2023-02-17 12:06:35 +0100

    [HAL][EXTI] Fix computation of pExtiConfig->GPIOSel in HAL_EXTI_GetConfigLine()

--- Src/stm32g0xx_hal_exti.c
+++ Src/stm32g0xx_hal_exti.c
@@ -343,7 +343,7 @@ HAL_StatusTypeDef HAL_EXTI_GetConfigLine(EXTI_HandleTypeDef *hexti, EXTI_ConfigT
       assert_param(IS_EXTI_GPIO_PIN(linepos));
 
       regval = EXTI->EXTICR[linepos >> 2u];
-      pExtiConfig->GPIOSel = ((regval << (EXTI_EXTICR1_EXTI1_Pos * (3uL - (linepos & 0x03u)))) >> 24);
+      pExtiConfig->GPIOSel = (regval >> (EXTI_EXTICR1_EXTI1_Pos * (linepos & 0x03u))) & EXTI_EXTICR1_EXTI0;
     }
   }
 
