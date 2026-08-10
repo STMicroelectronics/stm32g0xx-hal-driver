0ec1c071099f9e557d3ea25436fc085925bee11d
TrevorCash <cashtrevor@msn.com>
2026-08-10 12:38:36 -0600

    .

--- Src/stm32g0xx_hal_uart.c
+++ Src/stm32g0xx_hal_uart.c
@@ -3197,9 +3197,6 @@ HAL_StatusTypeDef UART_SetConfig(UART_HandleTypeDef *huart)
         pclk = (uint32_t) LSE_VALUE;
         break;
       case UART_CLOCKSOURCE_UNDEFINED:
-        pclk = 0U;
-        ret = HAL_ERROR;
-        break;
       default:
         pclk = 0U;
         ret = HAL_ERROR;
@@ -3253,9 +3250,6 @@ HAL_StatusTypeDef UART_SetConfig(UART_HandleTypeDef *huart)
         pclk = (uint32_t) LSE_VALUE;
         break;
       case UART_CLOCKSOURCE_UNDEFINED:
-        pclk = 0U;
-        ret = HAL_ERROR;
-        break;
       default:
         pclk = 0U;
         ret = HAL_ERROR;
@@ -3295,9 +3289,6 @@ HAL_StatusTypeDef UART_SetConfig(UART_HandleTypeDef *huart)
         pclk = (uint32_t) LSE_VALUE;
         break;
       case UART_CLOCKSOURCE_UNDEFINED:
-        pclk = 0U;
-        ret = HAL_ERROR;
-        break;
       default:
         pclk = 0U;
         ret = HAL_ERROR;
@@ -3724,8 +3715,6 @@ static void UART_DMATransmitCplt(DMA_HandleTypeDef *hdma)
   /* DMA Normal mode */
   if (HAL_IS_BIT_CLR(hdma->Instance->CCR, DMA_CCR_CIRC))
   {
-    huart->TxXferCount = 0U;
-
     /* Disable the DMA transfer for transmit request by resetting the DMAT bit
        in the UART CR3 register */
     ATOMIC_CLEAR_BIT(huart->Instance->CR3, USART_CR3_DMAT);
@@ -3776,8 +3765,6 @@ static void UART_DMAReceiveCplt(DMA_HandleTypeDef *hdma)
   /* DMA Normal mode */
   if (HAL_IS_BIT_CLR(hdma->Instance->CCR, DMA_CCR_CIRC))
   {
-    huart->RxXferCount = 0U;
-
     /* Disable PE and ERR (Frame error, noise error, overrun error) interrupts */
     ATOMIC_CLEAR_BIT(huart->Instance->CR1, USART_CR1_PEIE);
     ATOMIC_CLEAR_BIT(huart->Instance->CR3, USART_CR3_EIE);
@@ -3804,8 +3791,6 @@ static void UART_DMAReceiveCplt(DMA_HandleTypeDef *hdma)
      If Reception till IDLE event has been selected : use Rx Event callback */
   if (huart->ReceptionType == HAL_UART_RECEPTION_TOIDLE)
   {
-    huart->RxXferCount = 0;
-
     /* Check current nb of data still to be received on DMA side.
        DMA Normal mode, remaining nb of data will be 0
        DMA Circular mode, remaining nb of data is reset to RxXferSize */
